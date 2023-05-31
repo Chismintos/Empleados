79490c1d03e9c21620b69572a825926fe27b6e56
Cristian Garcia <cristianyahir538@gmail.com>
2023-05-30 20:18:40 -0700

    Proyecto Empleados

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,154 @@
-import Image from 'next/image'
+"use client"
+import React, { useState } from 'react';
 
-export default function Home() {
+
+export const HomePage = () => {
+  
+  const [nombre,setNombre] = useState(" ")
+  const [edad,setEdad] = useState(" ")
+  const [correo,setCorreo] = useState(" ")
+  const [genero,setGenero] = useState("Masculino")
+  const [resultados, setResultados] = useState(" ");
+  const [mostrarBoton, setMostrarBoton] = useState(false);
+  
+
+  const guardarDatos = (e: React.FormEvent) => {
+    e.preventDefault();
+  
+    setResultados(
+      'Nombre: ' + nombre + '\nEdad: ' + edad +'\nCorreo: ' + correo +'\nGenero: ' + genero
+    );
+  
+    setNombre('');
+    setEdad('');
+    setCorreo('');
+    setGenero('');
+  
+    setMostrarBoton(true);
+  };
+
+    function limpiar() {
+      setMostrarBoton(false);
+      setNombre('');
+      setEdad('');
+      setCorreo('');
+      setGenero('');
+      setResultados('');
+    }
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='p-6'>  
+
+      <header className='bg-blue-400 border border-transparent rounded-lg p-4 mb-5'>
+        <div className='flex justify-center'>
+          <div className='flex items-center'>
+            <img src='https://cdn.discordapp.com/emojis/978931095536025620.gif?size=96&quality=lossless' alt='GIF 1' className='w-8 h-8 mr-2' />
+            <p className='text-2xl uppercase text-white'>Empleados</p>
+            <img src='https://cdn.discordapp.com/emojis/978931095536025620.gif?size=96&quality=lossless' alt='GIF 2' className='w-8 h-8 ml-2' />
+          </div>
         </div>
-      </div>
+      </header>
+        
+      <section  className='bg-blue-200 border border-transparent rounded-lg p-4 mb-5 '  >
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <form onSubmit={guardarDatos}>
+            
+          <div className='flex flex-col mb-5'> 
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <label htmlFor="" className='mr-3 mb-3 ' >
+              Nombre
+            </label>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            <input 
+              type="text"
+              id='name'
+              className=' rounded-full w-56' //^ Ajusta el ancho de la caja input
+              value={nombre}
+              onChange={(event)=> setNombre(event.target.value)}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+            />
+          </div>
+        
+          <div className='flex flex-col mb-5'>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            <label htmlFor="" className='mr-3 mb-2'>
+              Edad
+            </label>
+
+            <input
+              type="text"
+              id='age'
+              className='rounded-full w-56' 
+              value={edad}
+              onChange={(event)=> setEdad(event.target.value)}
+            />
+
+          </div>
+
+          <div className='flex flex-col mb-5'> 
+
+            <label htmlFor="" className='mr-3 mb-2'>
+              Correo
+            </label>
+
+            <input
+              type="text"
+              id='email'
+              className='rounded-full w-56' //^ Ajusta el ancho de la caja input
+              value={correo}
+              onChange={(event)=> setCorreo(event.target.value)}
+
+            />
+          </div>
+
+          <div className='flex flex-col mb-5'> 
+
+            <label htmlFor="" className='mr-3 mb-2'>
+              Genero
+            </label>
+            
+            <select value={genero} id='edad' className='w-56' onChange={(event)=> setGenero(event.target.value)}>
+              <option value='Masculino'>Masculino</option>
+              <option value='Femenino'>Femenino</option>
+              <option value='Tanque de guerra'>Tanque de guerra</option>
+              <option value='No binario'>No binario</option>
+              <option value='Otro'>Otro</option>
+              
+            </select>  
+
+          </div>
+          <div className='p-2'> 
+
+            <input  
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded-full mr-9' 
+            onClick={guardarDatos}
+            type="submit" />
+
+              {/* Botón para limpiar los campos y agregar uno nuevo */}   
+
+            {mostrarBoton && (
+            <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-3 rounded-full' onClick={limpiar}>
+            Limpiar y agregar nuevo
+            </button>
+            )}
+
+          </div>
+        </form>
+
+          <pre className="mt-4 whitespace-pre-wrap mb-6">{resultados}</pre>
+        
+      </section>
+
+      <footer className='bg-blue-400 border border-transparent rounded-lg p-4 mb-5 flex items-center'>
+        <div>
+          Cristian Yahir Garcia Hernandez
+        </div>
+        <img src='https://cdn.discordapp.com/emojis/864111506635227136.gif?size=64' alt='GIF' className='w-9 h-9 ml-2' />
+      </footer>
+
     </main>
-  )
+  ) 
 }
+
+export default HomePage
+
